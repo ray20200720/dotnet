39a2d8325b0dc6665af6800739e46810dc1d263d
Ray Kuo <ray20140811@gmail.com>
2024-10-10 00:04:00 +0800

    update console/filewrite/Program.cs: WriteAllText and AppendText

--- console/filewrite/Program.cs
+++ console/filewrite/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.IO;
 
-string text = "Hello, world!";
+string text = "Hello, world!\nHow are you?\n";
 string filePath = "new_file.txt";
 
 using(StreamWriter writer = new StreamWriter(filePath))
@@ -8,3 +8,14 @@ using(StreamWriter writer = new StreamWriter(filePath))
     writer.WriteLine(text);
 }
 
+// writes the specified text to a file. If the file already exists, it overwrites its contents.
+text = "Some new contents here.\nDo you know what happen?\n";
+File.WriteAllText(filePath, text); 
+
+// append text to a file.
+text = "Append some text.";
+using(StreamWriter writer = File.AppendText(filePath))
+{
+    writer.WriteLine(text);
+}
+    
