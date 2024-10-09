d920e6fc251420027d37f582fb250574961116ef
Ray Kuo <ray20140811@gmail.com>
2024-10-09 23:07:45 +0800

    create file write example

---
+++ console/filewrite/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+string text = "Hello, world!";
+string filePath = "new_file.txt";
+
+using(StreamWriter writer = new StreamWriter(filePath))
+{
+    writer.WriteLine(text);
+}
+
