882baaf62014652b8fbb58cf7d2bc28dd6ddb1ae
Ray Kuo <ray20140811@gmail.com>
2024-10-09 23:13:38 +0800

    create read file example

---
+++ console/fileread/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+string filePath = "my_file.txt";
+using (StreamReader reader = new StreamReader(filePath))
+{
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+	Console.WriteLine(line);
+    }
+}
