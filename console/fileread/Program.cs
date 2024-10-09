1ab3153714d3bb8fbb0e47c6e054a61b1ff4e016
Ray Kuo <ray20140811@gmail.com>
2024-10-10 00:05:20 +0800

    update fileread

--- console/fileread/Program.cs
+++ console/fileread/Program.cs
@@ -3,9 +3,14 @@
 string filePath = "my_file.txt";
 using (StreamReader reader = new StreamReader(filePath))
 {
+    // reads a single line of text from a text reader object. 
     string? line;
     while ((line = reader.ReadLine()) != null)
     {
 	Console.WriteLine(line);
     }
+
+    // reads the entire content of a text file into a string. 
+    string text = File.ReadAllText(filePath);
+    Console.WriteLine(text);
 }
