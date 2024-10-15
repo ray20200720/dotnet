9053d2596095ae73a124a2a4bcad322ce48afb92
Ray Kuo <ray20200720@gmail.com>
2024-10-15 16:20:16 +0800

    create GET request with token example

---
+++ console/get/get_request_with_token/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            // 設定請求的 URL
+            string url = "https://api.example.com/data";
+
+            // 設定 Bearer Token
+            string token = "your_token_here";
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            // 發送 GET 請求
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            // 確認請求成功
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
